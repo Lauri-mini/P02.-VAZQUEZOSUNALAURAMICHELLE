c04c79bd8240d65df97331fe4f6cb845428ad9dc
Lauri-mini <60629484+Lauri-mini@users.noreply.github.com>
2020-05-06 17:05:50 -0700

    Initial commit

---
+++ ControlInventario/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlInventario.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int ProductCode { get; set; }
+        public string Productame { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+        public int SupplierId { get; set; }
+    }
+}
